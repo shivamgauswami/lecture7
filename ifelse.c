2845db870f183c42848cea3155a9f01108dd6466
shivamgauswami <130463011+shivamgauswami@users.noreply.github.com>
2023-04-14 17:37:01 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	int a=500,b=600;
+	if(a<=b)
+	{
+		printf("%d is minimum",a);
+	}
+	else
+	{
+		printf("%d is minimum",b);
+	}
+}
---
+++ ladder.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main()
+{
+	int a=-1;
+	if(a>1)
+	{
+		printf(" number is positive",a);
+	}
+	else if(a==0)
+	{
+		printf("number is neutral",a);
+	}
+	else if(a<0)
+	{
+		printf("number is nagetive",a);
+	}
+}
